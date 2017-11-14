69e6b84ff4262a52e4852029116676e7dfeb602c
AwesomeJiefu <33608345+AwesomeJiefu@users.noreply.github.com>
2017-11-13 19:44:43 -0500

    Add files via upload

---
+++ dt_author_id.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+
+"""
+    This is the code to accompany the Lesson 3 (decision tree) mini-project.
+
+    Use a Decision Tree to identify emails from the Enron corpus by author:
+    Sara has label 0
+    Chris has label 1
+"""
+
+import sys
+from time import time
+sys.path.append("../tools/")
+from email_preprocess import preprocess
+
+
+# features_train and features_test are the features for the training
+# and testing datasets, respectively
+# labels_train and labels_test are the corresponding item labels
+features_train, features_test, labels_train, labels_test = preprocess()
+
+
+#########################################################
+### your code goes here ###
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+
+length = len(features_train[0])
+
+# train the decision tree dmodel
+clf = DecisionTreeClassifier(min_samples_split=40)
+clf.fit(features_train, labels_train)
+
+# try to use the model to predict
+pred = clf.predict(features_test)
+
+# see accuracy
+accuracy = accuracy_score(pred, labels_test)
+print accuracy
+print length
+
+# you can change the features pencentile in email_preprocess.py, and then see the accuracy changes!
+#########################################################
